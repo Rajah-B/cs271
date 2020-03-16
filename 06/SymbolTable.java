8f53d806153f85797e8c3298bb08efdc8b7ef735
Rajah-B <55925625+Rajah-B@users.noreply.github.com>
2020-03-16 12:38:37 -0700

    Add files via upload

--- 06/SymbolTable.java
+++ 06/SymbolTable.java
@@ -1,35 +1,81 @@
+package assembler2;
 import java.util.HashMap;
 
-public class SymbolTable 
-{
-	private HashMap<String,Integer> sT = new HashMap<String,Integer>();
-	
-	public SymbolTable()
-	{
-		sT.put("R0", 0); sT.put("R1", 1); sT.put("R2", 2); sT.put("R3", 3); sT.put("R4", 4);
-		
-        sT.put("R5", 5); sT.put("R6", 6); sT.put("R7", 7); sT.put("R8", 8); sT.put("R9", 9);
-		
-        sT.put("R10", 10); sT.put("R11", 11); sT.put("R12", 12); sT.put("R13", 13); sT.put("R14", 14); sT.put("R15", 15);	
-		
-		sT.put("SP", 0); sT.put("LCL", 1); sT.put("ARG", 2); sT.put("THIS", 3);
-		
-        sT.put("THAT", 4); sT.put("SCREEN", 16384); sT.put("KBD", 24576);
-		
-	}
 
-	public void addEntry(String symbol, int address) 
-	{	
-		symbolTable.put(symbol, address);
-	}
-	
-	public boolean contains(String symbol)
-	{
-		return symbolTable.containsKey(symbol);
-	}
-	
-	public int getAddress(String symbol) 
-	{
-		return symbolTable.get(symbol);
-	}
+public class SymbolTable {
+
+    private HashMap<String, Integer> symTable;
+
+    private int index = 16;
+
+    public SymbolTable(){
+        symTable = new HashMap<String, Integer>();
+        symTable.put("R0", 0);
+		symTable.put("R1", 1);
+		symTable.put("R2", 2);
+		symTable.put("R3", 3);
+		symTable.put("R4", 4);
+		symTable.put("R5", 5);
+		symTable.put("R6", 6);
+		symTable.put("R7", 7);
+		symTable.put("R8", 8);
+		symTable.put("R9", 9);
+		symTable.put("R10", 10);
+		symTable.put("R11", 11);
+		symTable.put("R12", 12);
+		symTable.put("R13", 13);
+		symTable.put("R14", 14);
+		symTable.put("R15", 15);
+		
+		symTable.put("SP", 0);
+		symTable.put("LCL", 1);
+		symTable.put("ARG", 2);
+		symTable.put("THIS", 3);
+		symTable.put("THAT", 4);
+		symTable.put("SCREEN", 16384);
+		symTable.put("KBD", 24576);
+    }
+
+    /**
+     * L_command
+     * add (symbol,address) to symTable.
+     * if symtable contains symbol,add fail.
+     * @param symbol
+     * @param address
+     */
+    public void addEntry(String symbol,int address){
+        if(contains(symbol)){
+            return;
+        }
+        symTable.put(symbol, address);
+    }
+
+    /**
+     * add A_command symbol to symtable
+     * @param symbol
+     */
+    public void addEntry(String symbol){        
+        if(contains(symbol)){
+            return;
+        }
+        symTable.put(symbol, index++);
+    }
+
+    /**
+     * Whether it contains the specified symbol?
+     * @param symbol
+     * @return
+     */
+    public boolean contains(String symbol){
+        return symTable.containsKey(symbol);
+    }
+
+    /**
+     * getValue(key)
+     * @param symbol
+     * @return
+     */
+    public int getAddress(String symbol){
+        return symTable.get(symbol);
+    }
 }
